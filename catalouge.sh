aad9dd3c41ecdc97aa73fed2aab640d8e1915788
ashokkalathur <ashokkalathur@gmail.com>
2024-03-19 20:30:08 +0530

    add catalouge

---
+++ catalouge.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%T)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+MONGODB_HOST=mongodb.cloud6.online
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R failed $N"
+    else
+        echo -e "$2...$G success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+dnf module disable nodejs -y
+
+VALIDATE $? "disabling default nodejs" &>> $LOGFILE
+
+dnf module enable nodejs:18 -y
+
+VALIDATE $? "enableing nodejs:18" &>> $LOGFILE
+
+dnf install nodejs -y
+
+VALIDATE $? "installing NodeJs:18" &>> $LOGFILE
+
+useradd roboshop
+
+VALIDATE $? "creating roboshop user" &>> $LOGFILE
+
+mkdir /app
+
+VALIDATE $? "creating app directory" &>> $LOGFILE
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+
+VALIDATE $? "downloading catalouge application" &>> $LOGFILE
+
+cd /app 
+unzip /tmp/catalogue.zip
+
+VALIDATE $? "unzipping catalouge" &>> $LOGFILE
+
+npm install
+
+VALIDATE $? "installing dependencies" &>> $LOGFILE
+
+#use obsolute path
+cp /home/centos/roboshop-shell/catalouge.service /etc/systemd/system/catalogue.service
+
+VALIDATE $? "copying catalouge.service file" &>> $LOGFILE
+
+systemctl daemon-reload
+
+VALIDATE $? "catalouge demon reload" &>> $LOGFILE
+
+systemctl enable catalogue
+systemctl start catalogue
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org-shell -y
+
+mongo --host $MONGODB_HOST </app/schema/catalogue.js
+
+VALIDATE $? "loading data into mongoDB" &>> $LOGFILE
