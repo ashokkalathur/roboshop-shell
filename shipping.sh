64511181b1af9eb02c85968f317aa459edabf3fb
ashokkalathur <ashokkalathur@gmail.com>
2024-04-05 16:08:48 +0530

    add mysql and shipping

--- shipping.sh
+++ shipping.sh
@@ -30,7 +30,7 @@ else
     echo "you are root user"
 fi
 
-dnf install maven -y
+dnf install maven -y &>> $LOGFILE
 VALIDATE $? "installing maven"
 
 id roboshop
@@ -42,29 +42,41 @@ else
     echo -e "roboshop user already exist $Y SKIPPING $N"
 fi
 
-mkdir /app
+mkdir /app &>> $LOGFILE
 VALIDATE $? "creating app directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+version $? "downloading shipping"
 
 cd /app
+version $? "changing to app dir"
 
 unzip -o /tmp/shipping.zip
+version $? "unzipping shipping"
 
 mvn clean package
+version $? "installing dependencies"
 
 mv target/shipping-1.0.jar shipping.jar
+version $? "renaming jar file"
 
 cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+version $? "copying shipping  service"
 
 systemctl daemon-reload
+version $? "demon reload"
 
 systemctl enable shipping 
+version $? "enable shipping"
 
 systemctl start shipping
+version $? "start shipping"
 
 dnf install mysql -y
+version $? "installing mysql client"
 
 mysql -h mysql.cloud6.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+version $? "loading shipping database"
 
 systemctl restart shipping
+version $? "restarting shipping"
