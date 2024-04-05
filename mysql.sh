14ddb3e82b80299cdd963d32868fe19c6516905c
ashokkalathur <ashokkalathur@gmail.com>
2024-04-05 14:53:57 +0530

    add roboshop.conf

---
+++ mysql.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%T)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+MONGODB_HOST=mongodb.cloud6.online
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R failed $N"
+        exit 1
+    else
+        echo -e "$2...$G success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "disabling default nodejs"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "enableing nodejs:18"
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? "installing NodeJs:18" 
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "creating roboshop user"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
+
+dnf module disable mysql -y
+VALIDATE $? "disabling current mysql version"
+
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+VALIDATE $? "copied mysql.repo"
+
+dnf install mysql-community-server -y
+VALIDATE $? "Installing mysql server"
+
+systemctl enable mysqld
+VALIDATE $? "enabling mysql server"
+
+systemctl start mysqld
+VALIDATE $? "starting mysql server"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "setting mysql root passwd"
+
+
---
+++ shipping.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%T)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R failed $N"
+        exit 1
+    else
+        echo -e "$2...$G success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: please run this script with root access $N"
+    exit 1
+else
+    echo "you are root user"
+fi
